ddb32d0104393c3438da2f85104d16bb24beeee7
jasontromm <jason@trommetter.org>
2025-01-21 16:46:01 -0500

    Cleaned up main()

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from constants import *
 
 def main () :
     pygame.init()
-    print ("Starting asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
 
