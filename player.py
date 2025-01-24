9227b1c5607dfbefea7982093036910349a0539b
jasontromm <jason@trommetter.org>
2025-01-24 14:24:00 -0500

    This is the final game!

--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
-from circleshape import *
+from circleshape import CircleShape
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y) :
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -22,6 +24,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -34,6 +37,16 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move (-dt)
 
+        if keys[pygame.K_SPACE]:
+            self.shoot (dt)
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self, dt):
+        if (self.timer > 0) :
+            return
+        self.timer = PLAYER_SHOOT_COOLDOWN
+        shot = Shot (self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
